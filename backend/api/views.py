65104849be64532600c1fa08f523e6fb61352f21
Jaein Lewis <dlwodls0115@gmail.com>
2024-07-14 17:29:20 -0700

    SCRUM-8 Created view for creating a user

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
