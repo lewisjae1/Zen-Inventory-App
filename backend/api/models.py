b63ab93e8d553343fe53b9cf8500d59c9adc840f
Jaein Lewis <dlwodls0115@gmail.com>
2024-07-16 21:13:16 -0700

    SCRUM-7 Added expiration date to to order table and enabled additional message to have an empty value

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
 
 # Create your models here.
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
-    additionalMessage = models.CharField(max_length=100, null=True)
+    additionalMessage = models.CharField(max_length=100, null=True, blank=True)
+    expirationDate = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self.expirationDate:
+            self.expirationDate = timezone.now() + timedelta(days=90)
+        super().save(*args,**kwargs)
+
 
 class Product(models.Model):
     productName = models.CharField(max_length=50, null=False)
