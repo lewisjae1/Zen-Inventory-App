f739bf60a4ea6ff026eb8201b33c94407390d066
Jaein Lewis <dlwodls0115@gmail.com>
2024-07-25 15:54:55 -0700

    SCRUM-6 Added Serializers for Order model and Order Product model. I have two different serializers when updating order model because I want managers and workers to be able to update only certain elements according to their roles

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import *
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,29 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'date', 'additionalMessage', 'expirationDate', 'isCompleted']
+        extra_kwargs = {'user': {'read_only': True}}
+
+class OrderManagerUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['iscompleted']
+
+class OrderWorkerUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['additionalMessage']
+
+class OrderProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderProduct
+        fields = ['id','order', 'product', 'numProduct']
+
+class OrderProductUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderProduct
+        fields = ['id','numProduct']
