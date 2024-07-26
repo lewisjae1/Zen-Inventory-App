859ac7d6fd1de5cb4350f2ae36353c31696636ff
Jaein Lewis <dlwodls0115@gmail.com>
2024-07-26 11:39:28 -0700

    SCRUM-10 Got rid of unnecessary import

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,7 +3,6 @@ import api from '../api'
 import { useNavigate } from 'react-router-dom'
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'
 import '../styles/Form.css'
-import Register from '../pages/Register'
 
 function Form({route, method}) {
     const [username, setUsername] = useState('')
