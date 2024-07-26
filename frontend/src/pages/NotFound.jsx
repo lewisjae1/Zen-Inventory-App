784e87716ff97e1b092c3838fcada19c0c2a1840
Jaein Lewis <dlwodls0115@gmail.com>
2024-07-26 00:40:55 -0700

    SCRUM-9 renamed all class attributes to className

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,11 +2,11 @@ import '../styles/NotFound.css'
 
 function NotFound() {
     return <div>
-        <div class="number">404</div>
-        <div class="text"><span>Ooops...</span><br></br><span>이런...</span>
+        <div className="number">404</div>
+        <div className="text"><span>Ooops...</span><br></br><span>이런...</span>
         <br></br><br></br>
         page not found<br></br>없는 페이지 입니다</div>
-        <a class="me" href="https://codepen.io/uzcho_/pens/popular/?grid_type=list" target="_blank"></a>
+        <a className="me" href="https://codepen.io/uzcho_/pens/popular/?grid_type=list" target="_blank"></a>
     </div>
 }
 
