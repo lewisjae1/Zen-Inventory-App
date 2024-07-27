add043698815168e479882768b911ece3ff872bf
Jaein Lewis <dlwodls0115@gmail.com>
2024-07-27 13:04:59 -0700

    SCRUM-10 Added some codes to css file and Added elements that can lead user to login and register pages back and forth

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -7,10 +7,24 @@ import '../styles/Form.css'
 function Form({route, method}) {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [accessCode, setAccessCode] = useState('')
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
 
-    const name = method === 'login' ? 'Log In 로그인' : 'Register 유저 생성'
+    const name = method === 'login' ? 'Log In 로그인' : 'Register 계정 생성'
+    const buttonName = method === 'login' ? 'Register 계정 생성' : 'Log In 로그인'
+    const redirectPrompt1 = method === 'login' ? 'Don\'t Have an Account?' :
+            'Have an Account'
+    const redirectPrompt2 = method === 'login' ? '계정이 없나요?' :
+            '계정을 갖고 계신가요?'
+
+    const handleClick = () => {
+        if (method === 'login' ){
+            navigate('/register')
+        } else if (method === 'register') {
+            navigate('/login')
+        }
+    }
 
     const handleSubmit = async (e) => {
         setLoading(true)
@@ -20,7 +34,9 @@ function Form({route, method}) {
             if (method === 'register'){
                 if (password.match('[^0-9]') || password.length != 4){
                     alert('PIN Can Only Contain 4 Digits\n핀넘버는 4개의 숫자만을 사용해야합니다')
-                } else {
+                } else if (accessCode !== 'I love Zen'){
+                    alert('Incorrect Access Code\n맞지 않은 암호 입니다')
+                }else {
                     const res = await api.post(route, {username, password})
                     navigate('/login')
                 }
@@ -28,7 +44,7 @@ function Form({route, method}) {
                 const res = await api.post(route, {username, password})
                 localStorage.setItem(ACCESS_TOKEN, res.data.access)
                 localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
-                navigate('/')
+                navigate('/home')
             }   
         } catch (error) {
             if(method === 'register') {
@@ -42,12 +58,14 @@ function Form({route, method}) {
     }
 
     return <div className="login-box">
+        <h3>{redirectPrompt1}</h3>
+        <h3>{redirectPrompt2}</h3>
+        <button onClick={() => handleClick()} className="btn">{buttonName}</button>
         <form onSubmit={handleSubmit}>
             <h1>{name}</h1>
             <div className="user-box">
                 <input 
                     type='text'
-                    value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     required
                 />
@@ -56,13 +74,29 @@ function Form({route, method}) {
             <div className="user-box">
                 <input 
                     type='password'
-                    value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                 />
                 <label>PIN 핀번호</label>
-            </div><center>
-            <button type='submit'>
+            </div>
+            {method === 'register' &&
+                <div className="user-box">
+                <input 
+                    type='text'
+                    onChange={(e) => setAccessCode(e.target.value)}
+                    required
+                />
+                <label>Access Code 암호</label>
+                <span className='formSpan'>
+                    PIN Must be 4 Digits
+                </span>
+                <br />
+                <span className='formSpan'>
+                    핀번호는 4개의 숫자로 이루어져야 합니다
+                </span>
+                </div>
+            }<center>
+            <button className='formButton' type='submit'>
                     {name}
             </button></center>
         </form>
