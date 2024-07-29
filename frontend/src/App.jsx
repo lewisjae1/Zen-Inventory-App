b3e20828afcc1295f4d8ecce8abcab063c852ccb
Jaein Lewis <dlwodls0115@gmail.com>
2024-07-28 22:18:03 -0700

    SCRUM-10 Added header to the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import './styles/Render.css'
 import Login from './pages/Login'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import NotFound from './pages/NotFound'
 import ProtectedRoute from './components/ProtectedRoute'
-import Footer from './components/Footer'
 import Initial from './pages/Initial'
+import LoginStatus from './components/LoginStatus'
+import Footer from './components/Footer'
 
 function Logout() {
   localStorage.clear()
@@ -19,24 +21,25 @@ function RegisterAndLogout() {
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path='/'
-            element={<ProtectedRoute>
-              <Home />
-            </ProtectedRoute>} 
-          />
-          <Route path='/initial' element={<Initial />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/logout' element={<Logout />} />
-          <Route path='/register' element={<RegisterAndLogout />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
-    </div>
+    <>
+    <BrowserRouter>
+      <LoginStatus />
+      <Routes>
+        <Route
+          path='/'
+          element={<ProtectedRoute>
+            <Home />
+          </ProtectedRoute>} 
+        />
+        <Route path='/initial' element={<Initial />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/logout' element={<Logout />} />
+        <Route path='/register' element={<RegisterAndLogout />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+    <Footer />
+    </>
   )
 }
 
