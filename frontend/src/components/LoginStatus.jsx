05473a6d838b0552dc735e247d02ef961bce397c
Jaein Lewis <dlwodls0115@gmail.com>
2024-07-30 17:13:14 -0700

    SCRUM-10 Deleted unnecessary import

--- frontend/src/components/LoginStatus.jsx
+++ frontend/src/components/LoginStatus.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../styles/LoginStatus.css'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { fetchUserData } from '../utils/dataFetchutils'
