43a12572494f524b31ac98b8a7f9ef7f2230efcb
Jaein Lewis <dlwodls0115@gmail.com>
2024-08-01 23:22:56 -0700

    SCRUM-11 Added methods to fetch all existing user data and order data

--- frontend/src/utils/dataFetchutils.js
+++ frontend/src/utils/dataFetchutils.js
@@ -9,9 +9,27 @@ export const fetchUserData = async () => {
     }
 }
 
+export const fetchAllUsersData = async () => {
+    try {
+        const res = await api.get('api/allusers/')
+        return res.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 export const fetchProductData = async () => {
     try{
         const res = await api.get('api/product/')
+        return res.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const fetchOrderData = async () => {
+    try {
+        const res = await api.get('api/order/')
         return res.data
     } catch (error) {
         console.error(error)
