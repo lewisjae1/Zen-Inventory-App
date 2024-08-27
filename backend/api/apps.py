9c597e1d3469c1d8fbad5d6265692d11d02b49f9
Jaein Lewis <dlwodls0115@gmail.com>
2024-08-27 13:55:07 -0700

    Trying to see if replace can fix the error

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -10,11 +10,11 @@ class ApiConfig(AppConfig):
     name = 'api'
 
     def ready(self):
-        cred = credentials.Certificate(json.dumps({
+        cred = credentials.Certificate({
             "type": "service_account",
             "project_id": "zen-inventory-app",
             "private_key_id": os.environ['PRIVATE_KEY_ID'],
-            "private_key": os.environ['PRIVATE_KEY'],
+            "private_key": os.environ['PRIVATE_KEY'].replace('\\\\n', '\n'),
             "client_email": "firebase-adminsdk-t6kx7@zen-inventory-app.iam.gserviceaccount.com",
             "client_id": os.environ['CLIENT_ID'],
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -22,5 +22,5 @@ class ApiConfig(AppConfig):
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
             "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-t6kx7%40zen-inventory-app.iam.gserviceaccount.com",
             "universe_domain": "googleapis.com"
-        }))
+        })
         firebase_admin.initialize_app(cred)
