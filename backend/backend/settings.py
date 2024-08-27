3cd9edbd449c389617f81fcaab9d36393c29314b
Jaein Lewis <dlwodls0115@gmail.com>
2024-08-27 16:49:09 -0700

    Connection is failing. Creating element by calling config method and passing that as an argument.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -104,7 +104,8 @@ DATABASES = {
 
 import dj_database_url
 
-DATABASES['default'].update(dj_database_url.config(conn_max_age=600, ssl_require=True))
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
