cd7bdf24474245e5d9d56b928a650c0c2f96a535
Jaein Lewis <dlwodls0115@gmail.com>
2024-08-27 13:39:39 -0700

    cred couldn't deserialize elements, so using json.dumps method

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -3,13 +3,14 @@ import firebase_admin
 from firebase_admin import credentials
 from django.conf import settings
 import os
+import json
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
 
     def ready(self):
-        cred = credentials.Certificate({
+        cred = credentials.Certificate(json.dumps({
             "type": "service_account",
             "project_id": "zen-inventory-app",
             "private_key_id": os.environ['PRIVATE_KEY_ID'],
@@ -21,5 +22,5 @@ class ApiConfig(AppConfig):
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
             "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-t6kx7%40zen-inventory-app.iam.gserviceaccount.com",
             "universe_domain": "googleapis.com"
-        })
+        }))
         firebase_admin.initialize_app(cred)
